b9de914c1a643c4baf9c570d13a028d721391d6e
Guja <rnwkgur7@gmail.com>
2026-01-31 07:36:39 +0800

    update 260131

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 // vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import Sitemap from "vite-plugin-sitemap";
 
-export default defineConfig(({ mode }) => ({
-  plugins: [react()],
-  base: mode === "gh" ? "/ganada/" : "/",
-}));
+export default defineConfig({
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: "https://ganada.my",
+      dynamicRoutes: ["/", "/home", "/menu", "/promos", "/contact"],
+      // (선택) 제외하고 싶은 경로가 있으면:
+      // exclude: ["/admin", "/private"],
+      // (선택) robots.txt 생성 여부(기본 true):
+      // generateRobotsTxt: true,
+    }),
+  ],
+});
